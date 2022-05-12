fc5383078605ac1ff12c40b0b604f9870341f893
Andrei Birca <Andrei.Birca@endava.com>
2022-05-12 15:29:18 +0300

    initial commit

---
+++ src/main/java/com/endava/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.endava.springdemo.service;
+
+import java.util.List;
+
+import com.endava.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
